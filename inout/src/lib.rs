63cd6d559937a8b111c8bf1e3d2d6df4a7390903
Артём Павлов <newpavlov@gmail.com>
2021-11-23 14:58:48 +0300

    add inout crate

---
+++ inout/src/lib.rs
@@ -0,0 +1,21 @@
+//! Collection of custom reference types for code generic over in-place and
+//! buffer-to-buffer modes of operation.
+
+#![no_std]
+#![doc(
+    html_logo_url = "https://raw.githubusercontent.com/RustCrypto/media/8f1a9894/logo.svg",
+    html_favicon_url = "https://raw.githubusercontent.com/RustCrypto/media/8f1a9894/logo.svg",
+    html_root_url = "https://docs.rs/inout/0.1.0"
+)]
+#![allow(clippy::needless_lifetimes)]
+#![warn(missing_docs, rust_2018_idioms)]
+
+mod inout;
+mod intmpout;
+mod inout_buf;
+mod intmpout_buf;
+
+pub use crate::inout::InOut;
+pub use intmpout::InTmpOut;
+pub use inout_buf::{InOutBuf, InOutBufIter, InSrc, IntoArrayError, NotEqualError};
+pub use intmpout_buf::InTmpOutBuf;
